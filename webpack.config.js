b9afeaafa0a630d9b28278cf8494f869c3f25425
ShellyWu <shelly.wu@kangdainfo.com>
2020-07-05 22:51:58 +0800

    Init project

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+module.exports = {
+    entry: ['./app.jsx'],
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, './'),
+    },
+    module: {
+        rules: [
+            //第一個loader編譯JSX
+            { test: /.jsx$/, exclude: /node_modules/, use: { loader: 'babel-loader', options: { presets: ['@babel/preset-react', '@babel/preset-env'] } } },
+            //第二個loader編譯ES6
+            { test: /.js$/, exclude: /node_modules/, use: { loader: 'babel-loader', options: { presets: ['@babel/preset-env'] } } }
+        ]
+    },
+    devServer: {
+      port: 9000
+    }
+};
